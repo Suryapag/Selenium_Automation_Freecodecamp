962fe0bd9da98ee3f62e1d71dfd96b87c3e4bae6
Suryapag <suryapa9092@gmail.com>
2025-06-15 08:22:42 +0530

    Commit

--- src/test/java/Part1/LoginShouldFailTest.java
+++ src/test/java/Part1/LoginShouldFailTest.java
@@ -1,12 +1,15 @@
 package Part1;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-public class FirstSeleniumTest {
+public class LoginShouldFailTest {
 
     WebDriver driver;
 
